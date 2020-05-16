76080c857e3ce553f0ce435121f4d9f73209287e
Shakib Hossain <3992124+shakib609@users.noreply.github.com>
2020-05-16 13:48:19 +0600

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,13 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:todos/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('App renders smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(TodosApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Todos'), findsOneWidget);
+    expect(find.text('All'), findsOneWidget);
+    expect(find.text('Incomplete'), findsOneWidget);
+    expect(find.text('Complete'), findsOneWidget);
   });
 }
